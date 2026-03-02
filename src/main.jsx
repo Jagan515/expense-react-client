9c763395c749ca4ae94242a2a751593937947795
Jagan Pradhan <jaganp515@gmail.com>
2026-03-03 02:30:30 +0530

    Added axios

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,10 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 
+import axios from "axios";
 import App from "./App.jsx";
+
+axios.defaults.withCredentials = true;
 import { store } from "./store.js";
 
 import "bootstrap/dist/css/bootstrap.min.css";
